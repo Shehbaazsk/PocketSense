9559266889e8c7907a525a74bfa953559cd90aa4
shehbaazsk <shehbaazwebdev@gmail.com>
2025-01-05 14:06:04 +0530

    initial commit and added common model

---
+++ apps/utils/common_model.py
@@ -0,0 +1,30 @@
+from uuid import uuid4
+
+from django.db import models
+from rest_framework import serializers
+
+
+class CommonModel(models.Model):
+    """Abstract base model with common fields"""
+    
+    id = models.AutoField(primary_key=True, editable=False)
+
+    # INSTEAD OF EXPOSING PK WE CAN USE UUID
+    # uuid = models.UUIDField(default=uuid4, unique=True)
+
+    is_active = models.BooleanField(default=True)
+    is_delete = models.BooleanField(default=False)
+
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        abstract = True
+
+
+class CommonModelSerializer(serializers.ModelSerializer):
+    """Serializer for the CommonModel fields"""
+
+    class Meta:
+        # fields = ["uuid", "is_active", "is_delete", "created_at", "updated_at"]
+        fields = ["id", "is_active", "is_delete", "created_at", "updated_at"]
