b7d28ceca7fa5db72ce6f3ad45d46b2a542c7d26
shehbaazsk <shehbaazwebdev@gmail.com>
2025-01-11 12:40:24 +0530

    add monthly analysis api

--- apps/expenses/apis/api_views.py
+++ apps/expenses/apis/api_views.py
@@ -1,11 +1,14 @@
+from django.db.models import Sum
+from django.utils import timezone
 from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework import filters, status, viewsets
+from rest_framework.decorators import action
 from rest_framework.parsers import FormParser, MultiPartParser
 from rest_framework.permissions import IsAdminUser
 from rest_framework.response import Response
 
 from apps.expenses.filters import ExpenseFilter
-from apps.expenses.models import Expense
+from apps.expenses.models import Expense, ExpenseSplit
 from apps.expenses.serializers import ExpenseDetailSerializer, ExpenseSerializer
 from apps.utils.permissions import IsAdminOrOwner
 
@@ -44,3 +47,18 @@ class ExpenseViewSet(viewsets.ModelViewSet):
     def perform_destroy(self, instance):
         instance.is_delete = False
         instance.save()
+
+    @action(methods=['get'], detail=False, url_path='monthly-analysis')
+    def monthly_analysis(self, request):
+        month = request.query_params.get(
+            'month', timezone.now().strftime('%m'))
+        expenses = ExpenseSplit.objects.filter(
+            expense__date__month=int(month), user=request.user)
+        total_expense = expenses.aggregate(total=Sum('amount'))['total']
+        categories = expenses.values(
+            'expense__category').annotate(total=Sum('amount'))
+
+        return Response({
+            "total_expense": total_expense or 0,
+            "categories": categories
+        })
